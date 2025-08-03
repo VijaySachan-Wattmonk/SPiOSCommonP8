498476670a3bbb9cbff9a92036fda7204fd25d51
Vijay Sachan <vijaysachan@wattmonk.com>
2025-08-03 22:07:53 +0530

    --

--- Sources/SPiOSCommonP8/Code/Global/Global Functions.swift
+++ Sources/SPiOSCommonP8/Code/Global/Global Functions.swift
@@ -8,7 +8,7 @@ import Foundation
 
 
 let N_A="N/A"
-class Global{
+public class Global{
     
     public static let isSwiftUIPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
 }
