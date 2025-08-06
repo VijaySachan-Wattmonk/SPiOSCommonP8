ab8c0603984664b6f4c189ea4f95e3852cb415e8
Vijay Sachan <vijaysachan@wattmonk.com>
2025-08-06 16:14:40 +0530

    Fixed S3 part 1

--- Sources/SPiOSCommonP8/Code/Foundation/ErrorHandling/Common/FWError.swift
+++ Sources/SPiOSCommonP8/Code/Foundation/ErrorHandling/Common/FWError.swift
@@ -5,9 +5,12 @@
 //  Created by Vijay Sachan on 5/5/25.
 //
 import Foundation
-struct FWError: LocalizedError {
+public struct FWError: LocalizedError {
     let message: String
-    var errorDescription: String? {
+    public init(message: String) {
+        self.message = message
+    }
+    public var errorDescription: String? {
         return message
     }
 }
